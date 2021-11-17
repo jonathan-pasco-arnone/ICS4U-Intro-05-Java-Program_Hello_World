baa1a04baa466705a45d2d5fabe4e3d94c7601b1
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2021-11-17 11:55:37 -0500

    Delete GoodbyeWorld.java

--- GoodbyeWorld.java
+++
@@ -1,8 +0,0 @@
-public class GoodbyeWorld {
-
-    public static void main(String[] args) {
-        // Prints "Goodbye, World." to the terminal window.
-        System.out.println("Goodbye, World".);
-    }
-
-}
